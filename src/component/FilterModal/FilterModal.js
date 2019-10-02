e6e28c9b491f391402829e507bd371f7c306148a
JasonPob <jasonpob@gmail.com>
2019-10-02 15:08:47 -0400

    alvin came in fo jira

--- src/component/FilterModal/FilterModal.js
+++ src/component/FilterModal/FilterModal.js
@@ -1,115 +1,26 @@
 import React from 'react';
-import Auxe from '../../hoc/Auxe';
+import classes from './FilterModal.css';
 
-const FilterModal = (props) => {
-    return (
-        <Auxe>
-            <h3>Find Fans</h3>
-            <h3>Type</h3>
-            <div>
-                Use Type <label>
-                    <input
-                        type="radio"
-                        name="react-tips"
-                        value="Commercial"
-                        // checked={this.state.selectedOption === "commercial"}
-                        // onChange={this.handleOptionChange}
-                        className="form-check-input"
-                    />
-                    Commercial
-                </label>
+const FilterModal = (props) => (
+    <>
+    <div className={classes.type}>
+        <div className={classes.Label}>{props.category}</div>
+        <button 
+            className={classes.Less} 
+            onClick={props.added}>Commercial</button>
+        <button 
+            className={classes.More} 
+            onClick={props.added}>Industrial</button>
+        <button 
+            className={classes.More} 
+            onClick={props.added}>Residential</button>
+    </div>
 
-                <label>
-                    <input
-                        type="radio"
-                        name="react-tips"
-                        value="Industrial"
-                        // checked={this.state.selectedOption === "commercial"}
-                        // onChange={this.handleOptionChange}
-                        className="form-check-input"
-                    />
-                    Industrial
-                </label>
-
-                <label>
-                    <input
-                        type="radio"
-                        name="react-tips"
-                        value="Residential"
-                        // checked={this.state.selectedOption === "commercial"}
-                        // onChange={this.handleOptionChange}
-                        className="form-check-input"
-                    />
-                    Residential
-                </label>
-            </div>
-
-            <div>
-                Application  
-                <label>
-                    <input
-                        type="radio"
-                        name="react-tips"
-                        value="Indoor"
-                        // checked={this.state.selectedOption === "commercial"}
-                        // onChange={this.handleOptionChange}
-                        className="form-check-input"
-                    />
-                    Indoor
-                </label>
-                <label>
-                    <input
-                        type="radio"
-                        name="react-tips"
-                        value="Outdoor"
-                        // checked={this.state.selectedOption === "commercial"}
-                        // onChange={this.handleOptionChange}
-                        className="form-check-input"
-                    />
-                    Outdoor
-                </label>
-            </div>
-
-            <div>
-            Mounting Location
-            <label>
-                    <input
-                        type="radio"
-                        name="react-tips"
-                        value="Roof"
-                        // checked={this.state.selectedOption === "commercial"}
-                        // onChange={this.handleOptionChange}
-                        className="form-check-input"
-                    />
-                    Roof
-                </label>
-                <label>
-                    <input
-                        type="radio"
-                        name="react-tips"
-                        value="Wall"
-                        // checked={this.state.selectedOption === "commercial"}
-                        // onChange={this.handleOptionChange}
-                        className="form-check-input"
-                    />
-                    Wall
-                </label>
-                <label>
-                    <input
-                        type="radio"
-                        name="react-tips"
-                        value="freeStanding"
-                        // checked={this.state.selectedOption === "commercial"}
-                        // onChange={this.handleOptionChange}
-                        className="form-check-input"
-                    />
-                    Free standing
-                </label>
-                
-            </div>
-        </Auxe>
-    )
-
-};
+    <div className={classes.techDetails}>
+        <div className={classes.Label}>{props.label}</div>
+        
+    </div>
+    </>
+);
 
 export default FilterModal;
