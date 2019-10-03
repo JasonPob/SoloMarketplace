878340da19c7407fc05d3ee3a35b215dd6df3011
JasonPob <jasonpob@gmail.com>
2019-10-03 11:31:26 -0400

    process of changing state arrays into id/value pairs

--- src/component/FilterModal/FilterModal.js
+++ src/component/FilterModal/FilterModal.js
@@ -1,8 +1,12 @@
 import React, {Component} from 'react';
+import FilterButtons from './FilterButtons/FilterButtons';
+import FilterBoxes from './FilterBoxes/FilterBoxes';
 
 class FilterModal extends Component {
     state = { 
-        type:["Use Type", "Application", "Mounting Location", "Accessories", "Model year"],
+        type:[
+            {id:1, value:"Use Type" },
+             "Application", "Mounting Location", "Accessories", "Model year"],
             useType:["Commercial","Industrial","Residential"],
             application:["Indoor", "Outdoor"],
             mountingLocation:["Roof", "Wall", "Free standing"],
@@ -13,8 +17,17 @@ class FilterModal extends Component {
             
      }
     render() { 
+
         return ( 
             <div>
+                <div>Find fans</div>
+                <div>Type</div>
+                <div>
+                    Use Type: 
+                    {this.state.type.map(type =>(
+                        <FilterButtons key={useType}/>
+                    ))}
+                </div>
 
             </div>
          );
