ae5de59c03aa09c47b8f34be07732c3915f2637a
JasonPob <jasonpob@gmail.com>
2019-10-08 16:40:22 -0400

    I give up for the rest of the day

--- src/component/Filter/DynamicSliders.js
+++ src/component/Filter/DynamicSliders.js
@@ -1,52 +1,38 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import Slider, { Range } from 'rc-slider';
-// We can just import Slider or Range to reduce bundle size
-// import Slider from 'rc-slider/lib/Slider';
-// import Range from 'rc-slider/lib/Range';
-import 'rc-slider/assets/index.css';
- 
+import { makeStyles } from '@material-ui/core/styles';
+import Typography from '@material-ui/core/Typography';
+import Slider from '@material-ui/core/Slider';
 
-class DynamicSlider extends React.Component {
-        constructor(props) {
-          super(props);
-          this.state = {
-            lowerBound: 2000,
-            upperBound: 10000,
-            value: [0,5000],
-          };
-        }
-        onLowerBoundChange = (e) => {
-          this.setState({ lowerBound: +e.target.value });
-        }
-        onUpperBoundChange = (e) => {
-          this.setState({ upperBound: +e.target.value });
-        }
-        onSliderChange = (lowerBound, upperBound) => {
-          
-          this.setState({
-            lowerBound, upperBound
-          });
-        }
-        handleApply = () => {
-          const { lowerBound, upperBound } = this.state;
-          this.setState({ value: [lowerBound, upperBound] });
-        }
-        render() {
-            const labelStyle = { minWidth: '60px', display: 'inline-block' };
-          return (
-              <div className="column col-4 col-xs-12">
-              <label style={labelStyle}>Min Value: </label><span>{this.state.lowerBound}</span>
-              <br />
-              <label style={labelStyle}>Max Value: </label><span>{this.state.upperBound}</span>
-              
-              <br />
-              <br /><br />
-              <Range allowCross={false} value={this.state.value} onChange={this.onSliderChange} />
-            </div>
-          );
-        }
-      }
+const useStyles = makeStyles({
+  root: {
+    width: 300,
+  },
+});
 
+function valuetext(value) {
+  return `${value}`;
+}
 
-export default DynamicSlider;
+export default function DynamicSliders() {
+  const classes = useStyles();
+  const [value, setValue] = React.useState([20, 70]);
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
+  return (
+    <div className={classes.root}>
+      <Typography id="range-slider" gutterBottom>
+        Temperature range
+      </Typography>
+      <Slider
+        value={value}
+        onChange={handleChange}
+        valueLabelDisplay="auto"
+        aria-labelledby="range-slider"
+        getAriaValueText={valuetext}
+      />
+    </div>
+  );
+}
